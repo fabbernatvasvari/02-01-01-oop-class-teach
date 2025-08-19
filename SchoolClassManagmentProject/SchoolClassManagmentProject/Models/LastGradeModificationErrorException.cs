21f8dbf32565fda2ca91d11c7b6a4bb216dc6454
Csaba Gyuris <gyuris.csaba@vasvari.org>
2025-08-19 10:50:29 +0200

    Saját kivétel

---
+++ SchoolClassManagmentProject/SchoolClassManagmentProject/Models/LastGradeModificationErrorException.cs
@@ -0,0 +1,30 @@
+﻿
+namespace SchoolClassManagmentProject.Models
+{
+    [Serializable]
+    internal class LastGradeModificationErrorException : Exception
+    {
+        private string v1;
+        private string v2;
+        private object value;
+
+        public LastGradeModificationErrorException()
+        {
+        }
+
+        public LastGradeModificationErrorException(string? message) : base(message)
+        {
+        }
+
+        public LastGradeModificationErrorException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        public LastGradeModificationErrorException(string v1, string v2, object value)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.value = value;
+        }
+    }
+}
